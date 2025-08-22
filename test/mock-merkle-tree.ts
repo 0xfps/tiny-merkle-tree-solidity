82e13fdce4bc59d87e30ee5b5e70cb6e5bd7ae40
0xfps <anthony.nnaemeka.umeh@gmail.com>
2025-08-22 10:34:39 +0100

    refactor: drop openzeppelin for keccak256.

--- test/mock-merkle-tree.ts
+++ test/mock-merkle-tree.ts
@@ -1,5 +1,5 @@
-import { keccak256 } from "@openzeppelin/merkle-tree/dist/hashes";
 import { AbiCoder } from "ethers";
+import keccak256 from "keccak256";
 
 export function getRoot(leafNo: number): string {
     const coder = AbiCoder.defaultAbiCoder()
@@ -10,7 +10,7 @@ export function getRoot(leafNo: number): string {
 
     let leaves = leafs.map(function (leaf) {
         const encode = coder.encode(["string"], [leaf.toString()])
-        return keccak256(encode)
+        return hash(encode)
     })
 
     // Start tree with all the leaves.
@@ -30,7 +30,7 @@ export function getRoot(leafNo: number): string {
         // leaves to yield the root.
         if (length == 2) {
             concatLeaves = concatenateArrangedLeaves(leaves[0], leaves[1]);
-            hashedPairs.push(keccak256(concatLeaves));
+            hashedPairs.push(hash(concatLeaves));
             tree.unshift(hashedPairs);
             break;
         }
@@ -41,7 +41,7 @@ export function getRoot(leafNo: number): string {
         // grouping, it's not touched here.
         for (let i = 0; i < length - 1; i += 2) {
             concatLeaves = concatenateArrangedLeaves(leaves[i], leaves[i + 1])
-            hashedPairs.push(keccak256(concatLeaves));
+            hashedPairs.push(hash(concatLeaves));
         }
 
         // The leaf not touched in the loop as a result of the depth leaves being
@@ -60,15 +60,19 @@ export function getRoot(leafNo: number): string {
     return root
 }
 
+function hash(leaf: string): string {
+    return `0x${keccak256(leaf).toString("hex")}`
+}
+
 function concatenateArrangedLeaves(leaf1: string, leaf2: string) {
     if (leaf1 < leaf2)
         return Buffer.concat([
-            Buffer.from(leaf1.slice(2)),
+            Buffer.from(leaf1),
             Buffer.from(leaf2.slice(2))
         ]).toString();
 
     return Buffer.concat([
-        Buffer.from(leaf2.slice(2)),
+        Buffer.from(leaf2),
         Buffer.from(leaf1.slice(2))
     ]).toString();
 }
