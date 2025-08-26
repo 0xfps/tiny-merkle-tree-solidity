16ca959beaaccbcb6acf6e66dfda0af461260c1d
0xfps <anthony.nnaemeka.umeh@gmail.com>
2025-08-26 07:58:55 +0100

    test: add tests. chore: install TS merkle tree builder package.

--- test/mock-merkle-tree.ts
+++ test/mock-merkle-tree.ts
@@ -1,4 +1,5 @@
-import { AbiCoder, keccak256 } from "ethers";
+import MiniMerkleTree from "@fifteenfigures/mini-merkle-tree";
+import { AbiCoder, sha256 } from "ethers";
 
 export function getRoot(leafNo: number): string {
     const coder = AbiCoder.defaultAbiCoder()
@@ -9,65 +10,9 @@ export function getRoot(leafNo: number): string {
 
     let leaves = leafs.map(function (leaf) {
         const encode = coder.encode(["string"], [leaf.toString()])
-        return keccak256(encode)
+        return sha256(encode)
     })
 
-    // Start tree with all the leaves.
-    let tree = [leaves];
-    let length = leaves.length;
-
-    // All depths are stored in separate arrays.
-    // [[depth(n+1) or root], [depth(n)], ..., [depth0]].
-    // New leaves for new depths are recomputed based off of the
-    // previous ones.
-    while (length >= 2) {
-        let concatLeaves;
-        const hashedPairs = []
-
-        // When length == 2, which will eventually be so given
-        // the division applied at the end, it is the last two
-        // leaves to yield the root.
-        if (length == 2) {
-            concatLeaves = concatenateArrangedLeaves(leaves[0], leaves[1]);
-            hashedPairs.push(keccak256(concatLeaves));
-            tree.unshift(hashedPairs);
-            break;
-        }
-
-        // Iterate over the leaves in the array.
-        // length will always match leaves [ at depth].length;
-        // This loop runs in a way that if there's an extra leaf after
-        // grouping, it's not touched here.
-        for (let i = 0; i < length - 1; i += 2) {
-            concatLeaves = concatenateArrangedLeaves(leaves[i], leaves[i + 1])
-            hashedPairs.push(keccak256(concatLeaves));
-        }
-
-        // The leaf not touched in the loop as a result of the depth leaves being
-        // odd is simply moved to the next one.
-        if (length % 2 == 1) hashedPairs.push(leaves[length - 1])
-        
-            
-        // New leaf depths are stored in front.
-        tree.unshift(hashedPairs)
-        leaves = hashedPairs
-
-        length = Math.floor((length + 1) / 2)
-    }
-
-    const root = tree[0][0]
-    return root
-}
-
-function concatenateArrangedLeaves(leaf1: string, leaf2: string) {
-    if (leaf1 < leaf2)
-        return Buffer.concat([
-            Buffer.from(leaf1),
-            Buffer.from(leaf2.slice(2))
-        ]).toString();
-
-    return Buffer.concat([
-        Buffer.from(leaf2),
-        Buffer.from(leaf1.slice(2))
-    ]).toString();
+    const tree = new MiniMerkleTree(leaves)
+    return tree.root
 }
